af51f95e7b78d4f11b789edd7334cd282705431f
htobenothing <htobenothing@gmail.com>
2016-04-11 16:24:40 +0800

    first attempt

---
+++ FireBaseCrodova/FireBaseCrodova/www/scripts/login.js
@@ -0,0 +1,22 @@
+﻿var app = angular.module("sampleApp", ["firebase"]);
+app.controller('LoginCtrl', function ($rootScope, $scope, $state, $http, WebUrlService, userService) {
+    var datamodules = [];
+    $scope.Login = function () {
+        $scope.Login_Information = "";
+        var names = [];
+        var url = WebUrlService.url + "Login/" + $scope.username + "/" + $scope.password;
+        $http.get(url)
+            .then(function (response) {
+                if (response.data.LoginResult.Status) {
+                    datamodules = response.data.LoginResult.Modules;
+                    for (i = 0; i < datamodules.length ; i++) {
+                        names.push(datamodules[i].module_display_name.replace(/ +/g, ""));
+                    }
+                    $rootScope.$broadcast('refreshevent', names);
+                    $state.go(response.data.LoginResult.DefaultPage.replace(/ +/g, ""));
+                } else {
+                    $scope.Login_Information = " * Your username or password is wrong!";
+                }
+            });
+    }
+});
---
+++ FireBaseCrodova/FireBaseCrodova/www/scripts/route.js
@@ -0,0 +1,17 @@
+﻿
+app.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/company', {
+            templateUrl: 'pages/company.html',
+            controller: 'companyCtrl'
+        }).
+        when('/product', {
+            templateUrl: 'pages/product.html',
+            controller: 'productCtrl'
+        }).
+        otherwise({
+            redirectTo: '/maninCtrl'
+        });
+  }]);
+
